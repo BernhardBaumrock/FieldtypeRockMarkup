a7c24fd0774409129aab1a4a478aae02a252a08f
BernhardBaumrock <office@baumrock.com>
2019-08-23 15:16:07 +0200

    comment logs

--- InputfieldRockMarkup.js
+++ InputfieldRockMarkup.js
@@ -1,5 +1,5 @@
-console.log('loaded');
+// console.log('loaded');
 
 $(document).on('RockMarkup.init', function(e) {
-  console.log('triggered', $(e.target));
+//   console.log('triggered', $(e.target));
 });
